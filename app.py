29fbbedfe6fb9dc895864433529646edf321dd77
Yousef Saeed <yousafesaeed@gmail.com>
2023-11-05 08:53:16 +0200

    Use multi processing

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
+from multiprocessing import Process
 from time import sleep
 
 from database import *
 from views import *
 from api import *
 
-app.run()
 
-while True:
-    Shortener().delete_expired_urls()
-    sleep(60)
+def check_expired_urls():
+    with app.app_context():
+        shortener = Shortener()
+        while True:
+            shortener.delete_expired_urls()
+            sleep(60)
+
+
+if __name__ == "__main__":
+    p_check_expired_urls = Process(target=check_expired_urls)
+    p_check_expired_urls.start()
+    p_app = Process(target=app.run())
+    p_app.start()
